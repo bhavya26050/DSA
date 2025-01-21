1c47f8001969a75d4d8521a24853464db39e11cf
bhavya26050 <bhavyaverma435@gmail.com>
2025-01-21 23:25:35 +0530

    21/01/2025

---
+++ study/Map.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<map>
+#include<unordered_map>
+using namespace std;
+int main()
+{
+    //creation
+    // unordered_map<string , int> m;
+    map<string , int> m;
+
+    //insertion
+
+    //1
+    pair<string , int> p = make_pair("babbar" , 3);
+    m.insert(p);
+
+    //2
+    pair<string , int>pair2("love" , 2);
+    m.insert(pair2);
+
+    //3
+    m["mera"] = 1;
+
+    //searching
+    cout << m["mera"] << endl;
+    cout << m.at("babbar") << endl;
+
+    //cout<<m.at("Unknownkey") <<endl;
+    cout<<m["unknownkey"] << endl;
+
+    //size
+    cout << m.size() << endl;
+
+    //to check presence
+    cout<< m.count("bro") <<endl;
+
+    //erase
+    m.erase("love");
+    cout << m.size() << endl;
+
+    // for(auto i : m)
+    // {
+    //     cout<<i.first<<" "<<i.second<<endl;
+    // }    
+
+    //iterator
+    // unordered_map<string , int> :: iterator it = m.begin();
+    map<string , int> :: iterator it = m.begin();
+
+    while (it != m.end())
+    {
+        cout<<it -> first << " " << it -> second << endl;
+        it++;
+    }
+    
+
+
+    return 0;
+}
